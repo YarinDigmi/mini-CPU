9727be523869a8752f4f2a64c9e8f248969853f3
YarinDigmi <yarindigmi@proton.me>
2025-09-29 17:58:02 +0300

    Add files via upload

---
+++ src/components/rf.v
@@ -0,0 +1,22 @@
+module regfile(
+    input clk, areset, we,
+    input [2:0] raddr1,raddr2,waddr,
+    input [7:0] wdata,
+    output [7:0] rout1, rout2
+);
+
+reg [7:0]mem[0:7];
+integer i;
+
+always @(posedge areset)begin
+    if(areset) for(i=0;i<8;i++) mem[i]<=8'd0;
+end
+
+always @(negedge clk) begin
+    if(we) mem[waddr]<=wdata;
+end
+
+assign rout1=mem[raddr1];
+assign rout2=mem[raddr2];
+
+endmodule
